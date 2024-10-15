07c78fd92aef46b7794a6b078335c79051ab7d2f
marhjd <aussenstelle+github@posteo.de>
2024-10-15 12:32:48 +0200

    Add count_characters for text
    Return a dictionary of each char with an integer-count

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def main():
     path = "books/frankenstein.txt"
+
     text = get_text(path)
-    print(text)
-    print("wordcount: ", count_words(text))
+    print("text:", text)
+
+    wordcount = count_words(text)
+    print("wordcount:", wordcount)
+
+    charcount = count_characters(text)
+    print("charcount:", charcount)
 
 def get_text(path):
     with open(path, "r") as f:
@@ -12,6 +18,16 @@ def get_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    count = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in count:
+            count[lowered] += 1
+        else:
+            count[lowered] = 1
+    return count
 
 if __name__ == '__main__':
     main()
