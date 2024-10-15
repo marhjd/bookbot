ad7c420214c5e6423457a30ac541cc9a01cc1eb3
marhjd <aussenstelle+github@posteo.de>
2024-10-15 12:55:04 +0200

    Add report printing for text

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 def main():
     path = "books/frankenstein.txt"
 
-    text = get_text(path)
-    print("text:", text)
-
-    wordcount = count_words(text)
-    print("wordcount:", wordcount)
-
-    charcount = count_characters(text)
-    print("charcount:", charcount)
+    report = print_report(path)
+    print(report)
 
 def get_text(path):
     with open(path, "r") as f:
@@ -28,6 +22,25 @@ def count_characters(text):
         else:
             count[lowered] = 1
     return count
+
+def print_report(book_path):
+    text = get_text(book_path)
+
+    word_count = count_words(text)
+    charcount = count_characters(text)
+    charcount_sorted_desc = sorted(charcount.items(), key=lambda item: item[1], reverse=True)
+
+    charcount_summary = ""
+    for char_tuple in charcount_sorted_desc:
+        if char_tuple[0].isalpha():
+            charcount_summary += f"The '{char_tuple[0]}' character was found {char_tuple[1]} times\n"
+
+    charcount_summary += "--- End report ---"
+
+    return f"""--- Begin report of {book_path} ---
+{word_count} words found in the document
+
+{charcount_summary}"""
 
 if __name__ == '__main__':
     main()
